58cdd34d3e601d57616a09a095a99743ad63c292
Melvil <melvil@melvil-eeepc.(none)>
2012-09-04 13:24:38 +0900

    init RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Job::Application.config.secret_token = 'f098ca238bab4cf4b0275449bd7a39b98904f28d549996a8b65d379a2c04ecaadeebe01dbcdfe8e5243b6069a06a4c7a8b8f91480bc296e4b1130cf2398da2bf'
